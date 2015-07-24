f81326adc789594c479c35ce4d9c366d274caff3
MarkCommitB2 <m20brundieck@gmail.com>
2015-07-24 16:16:06 -0500

    versioned to 1.11

--- SentinelMainStartup/Program.cs
+++ SentinelMainStartup/Program.cs
@@ -3,86 +3,91 @@ using System.Configuration;
 using System.Diagnostics;
 using System.IO;
 
-    /// <summary>
-    /// Created by Mark Brundieck; July 2015
-    /// </summary>
+/// <summary>
+/// Created by Mark Brundieck; July 2015
+/// </summary>
 namespace SentinelMainStartup
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-           //support for command line startup, process args
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//support for command line startup, process args
             if (args.Length > 0)
-            {
-                ProcessInput(args);
-            }
-            else
-            {
-                OutputExpectedInput();
-                Console.WriteLine();
-                Converse();
-            }
-        }
-       
-        /// <summary>
-        /// recursively handle input from user and provide output as appropriate.
-        /// </summary>
+			{
+				ProcessInput(args);
+			}
+			else
+			{
+				OutputExpectedInput();
+				Console.WriteLine();
+				Converse();
+			}
+		}
+
+		/// <summary>
+		/// recursively handle input from user and provide output as appropriate.
+		/// </summary>
         private static void Converse()
-        { 
-            string inputLine = Console.ReadLine();
-            if (inputLine.Trim().Length > 0)
-            {
-                if (inputLine.ToLower().CompareTo("end")==0)
-                {
-                    EndThisProgram();
-                }
-                string[] inputArray = inputLine.Split(' ');
-                ProcessInput(inputArray);
-            }
-            // the only way out of this is to type "end"
+		{
+			string inputLine = Console.ReadLine();
+			if (inputLine.Trim().Length > 0)
+			{
+				if (inputLine.ToLower().CompareTo("end") == 0)
+				{
+					EndThisProgram();
+				}
+
+				string[] inputArray = inputLine.Split(' ');
+				ProcessInput(inputArray);
+			}
+
+			// the only way out of this is to type "end"
             Converse();
-        }
+		}
 
-        /// <summary>
-        /// process for ending this main startup program
-        /// </summary>
+		/// <summary>
+		/// process for ending this main startup program
+		/// </summary>
         private static void EndThisProgram()
-        {
-            Console.WriteLine("Ending Main application. Client and server need to be closed independently.");
-            Console.WriteLine();
-            System.Environment.Exit(-1);
+		{
+			Console.WriteLine("Ending Main application. Client and server need to be closed independently.");
+			Console.WriteLine();
+			System.Environment.Exit(-1);
+		}
 
-        }
-        /// <summary>
-        /// default instructions
-        /// </summary>
+		/// <summary>
+		/// default instructions
+		/// </summary>
         private static void OutputExpectedInput()
-        {
-            Console.WriteLine("Enter 'start server', 'start client', 'start both' or 'end'");
-        }
-        /// <summary>
-        /// Main routine for processing instructions
-        /// </summary>
-        /// <param name="args">array of arguments</param>
-        private static void ProcessInput(string[] args)
-        { 
-            switch (args.Length)
-            {
-                case 1:
-                    if (args[0].ToLower().CompareTo("end") == 0)
-                    {
-                        // Future enhancement: close client & server by finding the process and killing it.
-                        EndThisProgram();
-                    }
-                    else
-                    {
-                        OutputExpectedInput();
-                    }
+		{
+			Console.WriteLine("Enter 'start server', 'start client', 'start both' or 'end'");
+		}
 
-                    break;
-                case 2:
+		/// <summary>
+		/// Main routine for processing instructions
+		/// </summary>
+		/// <param name="args">array of arguments</param>
+        private static void ProcessInput(string[] args)
+		{
+			switch (args.Length)
+			{
+				case 1:
+                    if (args[0].ToLower().CompareTo("end") == 0)
+				{
+					// Future enhancement: close client & server by finding the process and killing it.
+                        EndThisProgram();
+				}
+				else
+				{
+					OutputExpectedInput();
+				}
+
+				break;
+				case 2:
                     ProcessStart(args[1]);
+					Console.WriteLine("Started " + args[1]);
+					Console.WriteLine();
                     break;
                 default:
                     OutputExpectedInput();
