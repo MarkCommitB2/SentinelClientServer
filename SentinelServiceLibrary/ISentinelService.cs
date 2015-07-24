6cef8680d306198459640a74d7832a0cb5d02c6c
MarkCommitB2 <m20brundieck@gmail.com>
2015-07-24 15:12:19 -0500

    initial quiz3 project for DMP

---
+++ SentinelServiceLibrary/ISentinelService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SentinelServiceLibrary
+{
+    [ServiceContract]
+    public interface ISentinelService
+    {
+        [OperationContract]
+        string GetData(string value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "default value";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
